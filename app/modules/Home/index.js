9dfda97c4fa64fa8dd18735a44f67a9ad94f77a2
tnrich <trich1230@gmail.com>
2016-01-07 16:28:09 -0800

    cleanup

--- app/modules/Home/index.js
+++ app/modules/Home/index.js
@@ -3,7 +3,7 @@ import { Decorator as Cerebral, Link } from 'cerebral-react';
 import Title from './components/Title';
 import InputWrapper from '../CerebralForm/InputWrapper';
 
-function MyInputComponent (props) {
+var MyInputComponent = InputWrapper(function MyInputComponent (props) {
   return (
     <div>
       is valid email?
@@ -14,34 +14,13 @@ function MyInputComponent (props) {
         </input>
     </div>
     );
-}
+}, {path: ['my','form','path'], validationName: 'email'})
 
-// var NewInput = InputWrapper((<div>Hey</div>), {path: ['uniquePath']})
-var MyInputComponent = InputWrapper(MyInputComponent, {path: ['uniquePath2'], validationName: 'email'})
-
-@Cerebral({
-  title: ['title'],
-  color: ['color']
-})
 class Home extends React.Component {
-
-  static propTypes = {
-    color: PropTypes.string,
-    title: PropTypes.string
-  };
-
   render() {
     return (
       <div>
-        <Title color={this.props.color}>{this.props.title}</Title>
-        <MyInputComponent> 
-        </MyInputComponent>  
-        {InputWrapper((props) =>( <input {...props}>
-                </input>), {path: ['uniquePath']})}
-
-        <button onClick={() => this.props.signals.colorChanged({color: 'red'})}>Red</button>
-        {' | '}
-        <button onClick={() => this.props.signals.colorChanged({color: 'blue'})}>Blue</button>
+        <MyInputComponent/> 
       </div>
     );
   }
